bcd261f7d1fe4fda5ea38cf52eecc87554a703f2
Stas Bes <forgame98@yandex.ru>
2023-06-28 21:27:47 +0300

    Added view bindinigs

--- app/src/main/java/com/mag/featurematching/MainActivity.kt
+++ app/src/main/java/com/mag/featurematching/MainActivity.kt
@@ -2,11 +2,17 @@ package com.mag.featurematching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mag.featurematching.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : MainActivityBinding
+    lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textMain.text = "awawdawd"
     }
 }
